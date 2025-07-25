d52bf1d9ef04359af69dc795d0c92740b2781eb9
fateme <117821877+efatemee@users.noreply.github.com>
2025-07-25 17:34:29 +0330

    creat twe home page section

---
+++ src/components/button/buttongreen.jsx
@@ -0,0 +1,49 @@
+import styled from "./buttongreen.module.css";
+function Buttongreen(props) {
+  return (
+    <button className={styled.buttongreen}>
+      <p>{props.title}</p>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+      >
+        <g
+          fill="none"
+          stroke="#fff"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+        >
+          <path
+            stroke-dasharray="64"
+            stroke-dashoffset="64"
+            d="M21 12c0 4.97 -4.03 9 -9 9c-4.97 0 -9 -4.03 -9 -9c0 -4.97 4.03 -9 9 -9c4.97 0 9 4.03 9 9Z"
+          >
+            <animate
+              fill="freeze"
+              attributeName="stroke-dashoffset"
+              dur="1.83s"
+              values="64;0"
+            />
+          </path>
+          <path
+            stroke-dasharray="6"
+            stroke-dashoffset="6"
+            d="M10 12l3 -3M10 12l3 3"
+          >
+            <animate
+              fill="freeze"
+              attributeName="stroke-dashoffset"
+              begin="2.135s"
+              dur="0.915s"
+              values="6;0"
+            />
+          </path>
+        </g>
+      </svg>
+    </button>
+  );
+}
+export default Buttongreen;
--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,6 +1,50 @@
 import Navbar from "../../components/navbar/navbar";
-
+import styled from "./home.module.css";
+import img from "../../assets/image/تصویر-شاخص-صفحه-اصلی.webp";
+import Bottongreen from "../../components/button/buttongreen";
+import Bottongray from "../../components/button/buttongray";
+import img2 from "../../assets/image/عکس-شاخص-صفحه-اصلی.webp";
 function Home() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <div className={styled.txtshop}>
+          <section>
+            <h1>
+              فروش انواع <span>محصولات</span> به قیمت عمده
+            </h1>
+            <p>
+              با پانزده یک فروشگاه توجیبته که هرزمان ، هر محصولی رو راحت و امن
+              خرید کنی
+              <br />
+              تا الان چیزی نگرفتی.! همین حالا اولین خریدت رو با تخفیف از فروشگاه
+              پانزده تجربه کن
+            </p>
+
+            <Bottongray title="بزن بریم برای تخفیف" />
+            <Bottongreen title="بزن بریم فروشگاه" />
+          </section>
+          <img src={img} alt="image products" />
+        </div>
+        <div className={styled.txt2shop}>
+          <section>
+            <div className={styled.moraba}>
+              <button></button>
+            <h1>
+              حس خوب <span>رسید </span>
+            </h1>
+            </div>
+            <p>
+              همه‌ی ما وقتی کالایی را به صورت اینترنتی سفارش میدهیم ، لحضه شماری
+              میکنیم تا بسته سالم و هرچه زود تر به دستمان برسد ، تیم ما تلاش
+              میکند تا حس و تجربه خوبی از خرید اینترنتی برایتان بسازد
+            </p>
+          </section>
+          <img src={img2} alt="image products" />
+        </div>
+      </div>
+    </>
+  );
 }
 export default Home;
